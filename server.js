23304fd225e5e6e418038636dde6889bf9739099
Jsegel <segeljosh@gmail.com>
2020-02-24 18:17:08 -0600

    adding server functionality and needed html files

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
 const express = require('express');
-const app = exrpess();
+const app = express();
+const PORT = process.env.PORT || 3000;
+var path = require('path');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get('/'), (req,res) => {
-    res.render('index.html')
-}
+app.get('/', (req,res) => {
+    res.sendFile(path.join(__dirname + '/index.html'))
+})
+
+app.get('/login', (req,res) =>{
+    res.sendFile(path.join(__dirname + '/login.html'))
+})
+
+app.get('/register', (req,res)=>{
+    res.sendFile(path.join(__dirname + '/register.html'))
+})
 
 app.listen(PORT, function () {
     console.log(`🌎  ==> API Server now listening on PORT HTTP://localhost:${PORT}!`);
